443c82583d1b42938aa99acebd64c96d3bb9d441
Chance Callahan <chance@chancecallahan.com>
2025-10-18 21:34:32 -0400

    vfdfgdfgdfg

--- rxconfig.py
+++ rxconfig.py
@@ -3,9 +3,6 @@ import os
 
 config = rx.Config(
     app_name="personalwww",
-    api_url=os.getenv("API_URL", "https://www.chancecallahan.com"),
-    deploy_url=os.getenv("DEPLOY_URL", "https://www.chancecallahan.com"),
-    backend_port=8080,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
